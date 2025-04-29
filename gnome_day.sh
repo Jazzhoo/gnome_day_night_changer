fc58271fa354188f34dc94e1571bd1608e22dc0f
Jan Szymanski <janohan@gmail.com>
2025-04-29 10:51:36 +0100

    Add cursor theme change

---
+++ gnome_day.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# gsettings set org.gnome.desktop.interface color-scheme prefer-light
+gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-blue'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus'
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Original-Ice'
+
+# changing starship preset
+rm ~/.config/starship.toml
+ln -s ~/Git/jlinux_settings/starship_presets/tokyo_day.toml ~/.config/starship.toml
---
+++ gnome_night.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-blue-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Original-Classic'
+
+# changing starship preset
+rm ~/.config/starship.toml
+ln -s ~/Git/jlinux_settings/starship_presets/tokyo_night.toml ~/.config/starship.toml
